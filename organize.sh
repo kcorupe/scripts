d6bfb0b5e0b3d9790bb449ba767d89c10ef3b428
kcorupe <kyle.corupe@gmail.com>
2012-07-23 15:29:16 -0700

    adding organize.sh

---
+++ organize.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+cd /Users/kyle.corupe/Downloads/
+
+echo "Organizing your SHIT!"
+echo ""
+mv -v *.pdf PDF/
+mv -v *.epub PDF/
+mv -v *.torrent Torrents/
+mv -v *.rpm RPMs/
+mv -v *.gz tgz/
+mv -v *gz tgz/
+mv -v *tbz tgz/
+mv -v *.dmg DMG/
+mv -v *.bin BIN/
+mv -v *.zip ZIP
+mv -v *.conf configs/
+mv -v *.iso ISOs/
+mv -v *.doc* DOCS/
+mv -v *.png png/
+mv -v *.txt txt/
+mv -v *.mp3 mp3/
+mv -v *.jpg jpg/
+mv -v *.exe exe/
+mv -v *.wav wav/
+mv -v *.xml xml/
+mv -v *.cfg config/
+mv -v *.cfg configs/
+mv -v *.jar jar/
+mv -v *.ini ini/
+mv -v *.py py/
+mv -v *.pl perl/
+mv -v *.csv csv/
+mv -v *.pkg DMG/
+mv -v *.sh scripts/
+mv -v *.xls xls/
+mv -v *.xls* xls/
+mv -v *.war war/
+mv -v *.jpeg jpg/
+
+echo ""
+echo "Done!"
